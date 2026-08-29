950d48cca422509dd49d2974f72675c8c953f4bd
varuntyagiai <varuntyagiai@gmail.com>
2026-08-29 15:05:41 +0530

    Add deployed frontend URL to backend CORS allowlist

--- project-4-fullstack-crud/backend/main.py
+++ project-4-fullstack-crud/backend/main.py
@@ -12,7 +12,8 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000",
+           "https://web-app-dev-mentorproject4.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
